b08571ae7d5cb2d05a2e179434e75247854ab162
Npsk698 <nsok64042@gmail.com>
2025-10-18 19:27:36 +0200

    build.gradle.kts

---
+++ coflix/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "Extension Fr by Narek"
+    language    = "fr"
+    authors = listOf("Narek")
+    status = 1
+    tvTypes = listOf("Movie","TvSeries")
+    iconUrl = "https://raw.githubusercontent.com/<TON_USER>/CloudStream-CoflixArt/main/CoflixArt/Icons/CoflixArt.png"
+    isCrossPlatform = true
+}
